084adaf27cc7d20eac0aa8066a894f1f28137c9f
tpaks <tpaks@DESKTOP-1NL0OLQ>
2024-07-15 16:24:21 +0900

    초기 커밋.

---
+++ wpftutorial-OpenFileDialog/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpftutorial_OpenFileDialog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            bool? success = fileDialog.ShowDialog();
+            fileDialog.Filter = "C# Success Files | *.cs";
+            fileDialog.Title = "Please pick CS Source file(s)...";
+            fileDialog.Multiselect = true;
+        
+            if(success == true)
+            {
+                string[] paths = fileDialog.FileNames;
+                string[] fileNames = fileDialog.SafeFileNames;
+                //tbInfo.Text = fileNames;
+            }
+            else
+            {
+                 
+            }
+
+        }
+    }
+}
